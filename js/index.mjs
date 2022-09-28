b0039bcea1f5a661e9be112f275499de96d9fdd4
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-09-28 19:44:42 +0200

    Newest Updates

--- js/index.mjs
+++ js/index.mjs
@@ -1,1 +1,21 @@
+const API_BASE_URL = "https://nf-api.onrender.com";
 
+const content = document.querySelector(".postContent");
+
+async function getPostContent() {
+  const getPostContentUrl = `${API_BASE_URL}/api/v1/social/posts`;
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+
+    console.log(results);
+
+    for (let i = 0; i < results.length; i++) {
+      content.innerHTML += `<a href="index.html.html?id=${results[i].id}"</a>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPostContent();
