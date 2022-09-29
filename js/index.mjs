39d6c2049035ddabb0769decb162144e23f3d5c0
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-09-29 18:35:05 +0200

    Newest Updates

--- js/index.mjs
+++ js/index.mjs
@@ -4,17 +4,30 @@ const content = document.querySelector(".postContent");
 
 async function getPostContent() {
   const getPostContentUrl = `${API_BASE_URL}/api/v1/social/posts`;
+
+  const token = localStorage.getItem("accessToken");
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
   try {
-    const response = await fetch(url);
+    const response = await fetch(getPostContentUrl, options);
     const results = await response.json();
 
     console.log(results);
 
     for (let i = 0; i < results.length; i++) {
-      content.innerHTML += `<a href="index.html.html?id=${results[i].id}"</a>`;
+      content.innerHTML += `<a href="index.html.html?id=${results[i].id}">
+      <div>${results[i].posts}</div>
+      </a>`;
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
 
